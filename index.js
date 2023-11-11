51b50269b6e82891e396926fe43bd255879efce8
Lara Mustafa <Laramustafa006@gmail.com>
2023-11-10 19:50:10 -0800

    added code to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const form = document.createElement('form')
+document.body.appendChild(form)
+
+const input = document.createElement('input')
+form.appendchild(ninput)
+
+constbutton = document.createElement('button')
+button.innertext = 'ADD'
+button.addEventListner('click,'(price)=> 50{
+    
+})
