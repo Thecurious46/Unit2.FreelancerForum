8cda12dfd9fa782cd4f09291cc142af48a0b3c90
Lara Mustafa <Laramustafa006@gmail.com>
2023-11-10 19:56:07 -0800

    added code

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ form.appendchild(ninput)
 constbutton = document.createElement('button')
 button.innertext = 'ADD'
 button.addEventListner('click,'(price)=> 50{
+    console.log (click to update)
     
 })
+
